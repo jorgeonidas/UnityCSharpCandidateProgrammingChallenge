6e29de14636f9ee2b122a254991f8ebac1b9fb41
Jorgeonidas <zangetsujm@gmail.com>
2020-08-08 14:44:10 -0500

    set gridlayout constrains

--- Assets/Scripts/DataDisplayer.cs
+++ Assets/Scripts/DataDisplayer.cs
@@ -10,22 +10,40 @@ public class DataDisplayer : MonoBehaviour
     public string fileRelativePath = "JsonChallenge.json";
     [Header("UI elements")]
     public Text tableTitleText;
+    public GridLayoutGroup gridLayoutGroup;
     TeamMembersModel teamMembers;
+    [Header("CellElement")]
+    public GameObject cellItem;
     // Start is called before the first frame update
     void Start()
     {
         teamMembers = FileManager.LoadModelFromJsonFile(fileRelativePath);
         if (teamMembers != null)
-        {
-
-        }
+            Fill(teamMembers);
         else
             Debug.LogError("Error loading data");
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void Fill(TeamMembersModel teamMembersModel)
     {
-        
+        ClearCells();
+        var headers = teamMembersModel.ColumnHeaders;
+        var data = teamMembersModel.Data;
+        gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayoutGroup.constraintCount = headers.Count;
+
+        //fill headers
+        for(int i = 0; i < headers.Count; i++)
+        {
+            Instantiate(cellItem, gridLayoutGroup.gameObject.transform);  
+        }
+    }
+
+    public void ClearCells()
+    {
+        foreach (Transform child in gridLayoutGroup.transform)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
